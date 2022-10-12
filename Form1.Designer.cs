8b70756d7a856c0547efa7571c372b98cbdef74c
Galaletsang Modimola <578204@student.belgiumcampus.ac.za>
2022-10-12 12:07:26 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TextBox textBox1;
+            System.Windows.Forms.TextBox textBox2;
+            System.Windows.Forms.TextBox textBox3;
+            System.Windows.Forms.TextBox textBox4;
+            System.Windows.Forms.TextBox textBox5;
+            System.Windows.Forms.TextBox textBox6;
+            System.Windows.Forms.TextBox textBox7;
+            System.Windows.Forms.TextBox textBox8;
+            System.Windows.Forms.TextBox textBox9;
+            this.btnExit = new System.Windows.Forms.Button();
+            textBox1 = new System.Windows.Forms.TextBox();
+            textBox2 = new System.Windows.Forms.TextBox();
+            textBox3 = new System.Windows.Forms.TextBox();
+            textBox4 = new System.Windows.Forms.TextBox();
+            textBox5 = new System.Windows.Forms.TextBox();
+            textBox6 = new System.Windows.Forms.TextBox();
+            textBox7 = new System.Windows.Forms.TextBox();
+            textBox8 = new System.Windows.Forms.TextBox();
+            textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox1.Location = new System.Drawing.Point(164, 162);
+            textBox1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(60, 60);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox2.Location = new System.Drawing.Point(224, 222);
+            textBox2.Margin = new System.Windows.Forms.Padding(0);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(60, 60);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox3.Location = new System.Drawing.Point(164, 222);
+            textBox3.Margin = new System.Windows.Forms.Padding(0);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new System.Drawing.Size(60, 60);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox4.Location = new System.Drawing.Point(284, 162);
+            textBox4.Margin = new System.Windows.Forms.Padding(0);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new System.Drawing.Size(60, 60);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox5.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox5.Location = new System.Drawing.Point(224, 162);
+            textBox5.Margin = new System.Windows.Forms.Padding(0);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new System.Drawing.Size(60, 60);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox6.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox6.Location = new System.Drawing.Point(284, 282);
+            textBox6.Margin = new System.Windows.Forms.Padding(0);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new System.Drawing.Size(60, 60);
+            textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            textBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox7.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox7.Location = new System.Drawing.Point(224, 282);
+            textBox7.Margin = new System.Windows.Forms.Padding(0);
+            textBox7.Multiline = true;
+            textBox7.Name = "textBox7";
+            textBox7.Size = new System.Drawing.Size(60, 60);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox8.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox8.Location = new System.Drawing.Point(164, 282);
+            textBox8.Margin = new System.Windows.Forms.Padding(0);
+            textBox8.Multiline = true;
+            textBox8.Name = "textBox8";
+            textBox8.Size = new System.Drawing.Size(60, 60);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            textBox9.Cursor = System.Windows.Forms.Cursors.Default;
+            textBox9.Location = new System.Drawing.Point(284, 222);
+            textBox9.Margin = new System.Windows.Forms.Padding(0);
+            textBox9.Multiline = true;
+            textBox9.Name = "textBox9";
+            textBox9.Size = new System.Drawing.Size(60, 60);
+            textBox9.TabIndex = 8;
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Location = new System.Drawing.Point(491, 295);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(106, 47);
+            this.btnExit.TabIndex = 9;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(textBox9);
+            this.Controls.Add(textBox8);
+            this.Controls.Add(textBox7);
+            this.Controls.Add(textBox6);
+            this.Controls.Add(textBox5);
+            this.Controls.Add(textBox4);
+            this.Controls.Add(textBox3);
+            this.Controls.Add(textBox2);
+            this.Controls.Add(textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
