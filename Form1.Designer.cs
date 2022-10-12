4f0c6d706c42d9286af2ecb510e10ab38917eb70
Galaletsang Modimola <578204@student.belgiumcampus.ac.za>
2022-10-12 17:06:29 +0200

    Game is fully functional

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,151 +29,194 @@ namespace Tic_Tac_Toe
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TextBox textBox1;
-            System.Windows.Forms.TextBox textBox2;
-            System.Windows.Forms.TextBox textBox3;
-            System.Windows.Forms.TextBox textBox4;
-            System.Windows.Forms.TextBox textBox5;
-            System.Windows.Forms.TextBox textBox6;
-            System.Windows.Forms.TextBox textBox7;
-            System.Windows.Forms.TextBox textBox8;
-            System.Windows.Forms.TextBox textBox9;
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox5 = new System.Windows.Forms.TextBox();
+            this.txtBox4 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox9 = new System.Windows.Forms.TextBox();
+            this.txtBox8 = new System.Windows.Forms.TextBox();
+            this.txtBox7 = new System.Windows.Forms.TextBox();
+            this.txtBox6 = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
-            textBox1 = new System.Windows.Forms.TextBox();
-            textBox2 = new System.Windows.Forms.TextBox();
-            textBox3 = new System.Windows.Forms.TextBox();
-            textBox4 = new System.Windows.Forms.TextBox();
-            textBox5 = new System.Windows.Forms.TextBox();
-            textBox6 = new System.Windows.Forms.TextBox();
-            textBox7 = new System.Windows.Forms.TextBox();
-            textBox8 = new System.Windows.Forms.TextBox();
-            textBox9 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtBox1
             // 
-            textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox1.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox1.Location = new System.Drawing.Point(164, 162);
-            textBox1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
-            textBox1.Multiline = true;
-            textBox1.Name = "textBox1";
-            textBox1.Size = new System.Drawing.Size(60, 60);
-            textBox1.TabIndex = 0;
+            this.txtBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox1.HideSelection = false;
+            this.txtBox1.Location = new System.Drawing.Point(164, 162);
+            this.txtBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.ReadOnly = true;
+            this.txtBox1.Size = new System.Drawing.Size(60, 60);
+            this.txtBox1.TabIndex = 0;
+            this.txtBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox1.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox2
+            // txtBox5
             // 
-            textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox2.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox2.Location = new System.Drawing.Point(224, 222);
-            textBox2.Margin = new System.Windows.Forms.Padding(0);
-            textBox2.Multiline = true;
-            textBox2.Name = "textBox2";
-            textBox2.Size = new System.Drawing.Size(60, 60);
-            textBox2.TabIndex = 1;
+            this.txtBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox5.Location = new System.Drawing.Point(224, 222);
+            this.txtBox5.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox5.Multiline = true;
+            this.txtBox5.Name = "txtBox5";
+            this.txtBox5.ReadOnly = true;
+            this.txtBox5.Size = new System.Drawing.Size(60, 60);
+            this.txtBox5.TabIndex = 1;
+            this.txtBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox5.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox3
+            // txtBox4
             // 
-            textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox3.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox3.Location = new System.Drawing.Point(164, 222);
-            textBox3.Margin = new System.Windows.Forms.Padding(0);
-            textBox3.Multiline = true;
-            textBox3.Name = "textBox3";
-            textBox3.Size = new System.Drawing.Size(60, 60);
-            textBox3.TabIndex = 2;
+            this.txtBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox4.Location = new System.Drawing.Point(164, 222);
+            this.txtBox4.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox4.Multiline = true;
+            this.txtBox4.Name = "txtBox4";
+            this.txtBox4.ReadOnly = true;
+            this.txtBox4.Size = new System.Drawing.Size(60, 60);
+            this.txtBox4.TabIndex = 2;
+            this.txtBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox4.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox4
+            // txtBox3
             // 
-            textBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox4.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox4.Location = new System.Drawing.Point(284, 162);
-            textBox4.Margin = new System.Windows.Forms.Padding(0);
-            textBox4.Multiline = true;
-            textBox4.Name = "textBox4";
-            textBox4.Size = new System.Drawing.Size(60, 60);
-            textBox4.TabIndex = 3;
+            this.txtBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox3.Location = new System.Drawing.Point(284, 162);
+            this.txtBox3.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox3.Multiline = true;
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.ReadOnly = true;
+            this.txtBox3.Size = new System.Drawing.Size(60, 60);
+            this.txtBox3.TabIndex = 3;
+            this.txtBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox3.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox5
+            // txtBox2
             // 
-            textBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox5.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox5.Location = new System.Drawing.Point(224, 162);
-            textBox5.Margin = new System.Windows.Forms.Padding(0);
-            textBox5.Multiline = true;
-            textBox5.Name = "textBox5";
-            textBox5.Size = new System.Drawing.Size(60, 60);
-            textBox5.TabIndex = 4;
+            this.txtBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox2.Location = new System.Drawing.Point(224, 162);
+            this.txtBox2.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.ReadOnly = true;
+            this.txtBox2.Size = new System.Drawing.Size(60, 60);
+            this.txtBox2.TabIndex = 4;
+            this.txtBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox2.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox6
+            // txtBox9
             // 
-            textBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox6.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox6.Location = new System.Drawing.Point(284, 282);
-            textBox6.Margin = new System.Windows.Forms.Padding(0);
-            textBox6.Multiline = true;
-            textBox6.Name = "textBox6";
-            textBox6.Size = new System.Drawing.Size(60, 60);
-            textBox6.TabIndex = 5;
+            this.txtBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox9.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox9.Location = new System.Drawing.Point(284, 282);
+            this.txtBox9.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox9.Multiline = true;
+            this.txtBox9.Name = "txtBox9";
+            this.txtBox9.ReadOnly = true;
+            this.txtBox9.Size = new System.Drawing.Size(60, 60);
+            this.txtBox9.TabIndex = 5;
+            this.txtBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox9.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox7
+            // txtBox8
             // 
-            textBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox7.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox7.Location = new System.Drawing.Point(224, 282);
-            textBox7.Margin = new System.Windows.Forms.Padding(0);
-            textBox7.Multiline = true;
-            textBox7.Name = "textBox7";
-            textBox7.Size = new System.Drawing.Size(60, 60);
-            textBox7.TabIndex = 6;
+            this.txtBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox8.Location = new System.Drawing.Point(224, 282);
+            this.txtBox8.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox8.Multiline = true;
+            this.txtBox8.Name = "txtBox8";
+            this.txtBox8.ReadOnly = true;
+            this.txtBox8.Size = new System.Drawing.Size(60, 60);
+            this.txtBox8.TabIndex = 6;
+            this.txtBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox8.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox8
+            // txtBox7
             // 
-            textBox8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox8.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox8.Location = new System.Drawing.Point(164, 282);
-            textBox8.Margin = new System.Windows.Forms.Padding(0);
-            textBox8.Multiline = true;
-            textBox8.Name = "textBox8";
-            textBox8.Size = new System.Drawing.Size(60, 60);
-            textBox8.TabIndex = 7;
+            this.txtBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox7.Location = new System.Drawing.Point(164, 282);
+            this.txtBox7.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox7.Multiline = true;
+            this.txtBox7.Name = "txtBox7";
+            this.txtBox7.ReadOnly = true;
+            this.txtBox7.Size = new System.Drawing.Size(60, 60);
+            this.txtBox7.TabIndex = 7;
+            this.txtBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox7.Click += new System.EventHandler(this.BoxClick);
             // 
-            // textBox9
+            // txtBox6
             // 
-            textBox9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            textBox9.Cursor = System.Windows.Forms.Cursors.Default;
-            textBox9.Location = new System.Drawing.Point(284, 222);
-            textBox9.Margin = new System.Windows.Forms.Padding(0);
-            textBox9.Multiline = true;
-            textBox9.Name = "textBox9";
-            textBox9.Size = new System.Drawing.Size(60, 60);
-            textBox9.TabIndex = 8;
+            this.txtBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBox6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox6.Location = new System.Drawing.Point(284, 222);
+            this.txtBox6.Margin = new System.Windows.Forms.Padding(0);
+            this.txtBox6.Multiline = true;
+            this.txtBox6.Name = "txtBox6";
+            this.txtBox6.ReadOnly = true;
+            this.txtBox6.Size = new System.Drawing.Size(60, 60);
+            this.txtBox6.TabIndex = 8;
+            this.txtBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtBox6.Click += new System.EventHandler(this.BoxClick);
             // 
             // btnExit
             // 
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnExit.Location = new System.Drawing.Point(491, 295);
+            this.btnExit.Location = new System.Drawing.Point(534, 295);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(106, 47);
             this.btnExit.TabIndex = 9;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(492, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 31);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "First Players Turn";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExit);
-            this.Controls.Add(textBox9);
-            this.Controls.Add(textBox8);
-            this.Controls.Add(textBox7);
-            this.Controls.Add(textBox6);
-            this.Controls.Add(textBox5);
-            this.Controls.Add(textBox4);
-            this.Controls.Add(textBox3);
-            this.Controls.Add(textBox2);
-            this.Controls.Add(textBox1);
+            this.Controls.Add(this.txtBox6);
+            this.Controls.Add(this.txtBox7);
+            this.Controls.Add(this.txtBox8);
+            this.Controls.Add(this.txtBox9);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox3);
+            this.Controls.Add(this.txtBox4);
+            this.Controls.Add(this.txtBox5);
+            this.Controls.Add(this.txtBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -184,6 +227,16 @@ namespace Tic_Tac_Toe
         #endregion
 
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox5;
+        private System.Windows.Forms.TextBox txtBox4;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox9;
+        private System.Windows.Forms.TextBox txtBox8;
+        private System.Windows.Forms.TextBox txtBox7;
+        private System.Windows.Forms.TextBox txtBox6;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,89 @@ namespace Tic_Tac_Toe
 {
     public partial class Form1 : Form
     {
+        private static int count = 0;
+        private static string player1 = "X";
+        private static string player2 = "O";
+        private string thisplayer;
+
         public Form1()
         {
             InitializeComponent();
+            Text = "Tic Tac Toe";
         }
+
+        private string CurrentPlayer()
+        {
+            if (count % 2 == 0)
+            {
+                count++;
+                label1.Text = "Second Players Turn";
+                thisplayer = player1;
+                return player1;
+            } 
+            else
+            {
+                count++;
+                label1.Text = "First Players Turn";
+                thisplayer = player2;
+                return player2;
+            }
+        } 
+
+        private void CheckWinner()
+        {
+            if (txtBox1.Text == thisplayer && txtBox5.Text == thisplayer && txtBox9.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if(txtBox3.Text == thisplayer && txtBox5.Text == thisplayer && txtBox7.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if (txtBox1.Text == thisplayer && txtBox2.Text == thisplayer && txtBox3.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if (txtBox4.Text == thisplayer && txtBox5.Text == thisplayer && txtBox6.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if (txtBox7.Text == thisplayer && txtBox8.Text == thisplayer && txtBox9.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if (txtBox1.Text == thisplayer && txtBox4.Text == thisplayer && txtBox7.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if (txtBox2.Text == thisplayer && txtBox5.Text == thisplayer && txtBox8.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+            if (txtBox3.Text == thisplayer && txtBox6.Text == thisplayer && txtBox9.Text == thisplayer)
+            {
+                MessageBox.Show("You're the Winner");
+            }
+        }
+
+        private void BoxClick(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(sender.GetType().GetProperty("Text").GetValue(sender).ToString()))
+            {
+                sender.GetType().GetProperty("Text").SetValue(sender, CurrentPlayer());
+                CheckWinner();
+            }
+            else
+            {
+                MessageBox.Show("Pick another spot");
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
     }
 }
